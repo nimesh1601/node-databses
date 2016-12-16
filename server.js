63ff7f530c6a72702638fde1365020f7d1ed5a77
nimesh1601 <nimesh1601@gmail.com>
2016-12-16 15:14:15 +0530

    Add post todos

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/users');
+var {Todo} = require('./models/todos');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+
+	var todo = new Todo({
+		text : req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc)
+	},(e) =>{
+		res.status(404).send(e);
+	});
+});
+
+
+
+app.listen(3000,()=>
+{
+	console.log('Starting at port 3000');
+});
